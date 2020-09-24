3672ead1b5b978b043636fe48b6d4f109c465bfe
Claudio Garaycochea <computadoraweb@gmail.com>
2020-09-24 00:17:36 -0300

    Loading

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,38 +2,60 @@ import React, { useRef, useState } from 'react';
 import { WebView } from 'react-native-webview';
 import { StyleSheet, Text, View } from 'react-native';
 import Loading from '../../ui/loading/Loading';
+/*import { NativeRouter, Route, Link } from "react-router-native";
+
+const Home = () => <Text>Home</Text>;
+
+const About = () => <Text>About</Text>;*/
 
 export default function Home() {
   const [visible, setVisible] = useState(true);
+  const [loadProgress, setLoadProgress] = useState(0);
   const webViewRef = useRef(null);
+  // console.log('Home: webViewRef:', webViewRef);
+  function processHeader(e, loadProgress) {
+    console.log(e);
+    const { url } = e;
+    if((url==='https://test.projectbloc.io/login')&& loadProgress===1) {
+      console.log('open loading', loadProgress);
+    }
+  }
+
   return (
     <View style={{ flex: 1 }}>
+      {visible ? (
+        <Loading progress={loadProgress}/>
+      ) : null}
       <WebView
-        style={styles.container}
+        style={visible ? styles.containerA : styles.containerB}
+        height={0}
         ref={webViewRef}
         scrollEnabled={true}
         onLoad={() => setVisible(false)}
+        onLoadProgress={e => setLoadProgress(e.nativeEvent.progress)}
+        onNavigationStateChange={e => processHeader(e, loadProgress)}
         // overScrollMode="never"
         source={{
-          uri: `https://projectbloc.io/login`,
+          uri: `https://test.projectbloc.io/login`,
         }}
       />
-      {visible ? (
-        <Loading/>
-      ) : null}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    width: '50%',
+  containerA: {
+    width: 0,
+    height: 0,
     backgroundColor: '#192744',
     alignItems: 'center',
     justifyContent: 'center',
-    borderWidth: 2,
-    borderStyle:  'dashed',
-    borderColor: 'red',
+  },
+  containerB: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: '#192744',
+    alignItems: 'center',
+    justifyContent: 'center',
   }
 });
--- src/ui/loading/Loading.js
+++ src/ui/loading/Loading.js
@@ -1,15 +1,28 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-// import styled from 'styled-components';
+import { StyleSheet, View, Text } from 'react-native';
+import Bg from "../../../assets/loading.svg";
+import { ProgressBar, Colors } from 'react-native-paper';
 
 export default function Loading(props) {
+  const { progress } = props;
   return (
-    <View
-      style={{
-        backgroundColor: 'red'
-      }}
-    >
-      <Text>Loading..............</Text>
+    <View style={styles.container}>
+      <Bg width={"48px"} height={"48px"} />
+      <ProgressBar progress={progress} />
+      <Text style={[styles.setFontSize,styles.setColorWhite]}>Loading... {parseFloat(progress).toFixed(2)*100+"%"}</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: '#192744',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  setColorWhite : {
+    color: '#FFFFFF'
+  },
+});
