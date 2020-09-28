9e7e8a95f52a50af02884b91f4d90cab3543c07a
Claudio Garaycochea <computadoraweb@gmail.com>
2020-09-28 14:08:05 -0300

    Expo cli

--- src/ui/loading/Loading.js
+++ src/ui/loading/Loading.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-import Bg from "../../../assets/loading.svg";
+// import Bg from "../../../assets/loading.svg";
 import { ProgressBar, Colors } from 'react-native-paper';
 
 export default function Loading(props) {
   const { progress } = props;
   return (
     <View style={styles.container}>
-      <Bg width={"48px"} height={"48px"} />
+      {/*<Bg width={"48px"} height={"48px"} />*/}
       <ProgressBar progress={progress} />
       <Text style={[styles.setFontSize,styles.setColorWhite]}>Loading... {parseFloat(progress).toFixed(2)*100+"%"}</Text>
     </View>
